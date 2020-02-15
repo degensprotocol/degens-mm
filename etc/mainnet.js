d564af5fcc22ebe2915cc10137e3b7b1626b8d48
Degens Dev Team <support@degens.com>
2020-02-15 00:31:17 -0500

    show address for WETH in default config

--- etc/mainnet.js
+++ etc/mainnet.js
@@ -15,7 +15,8 @@ module.exports = {
 
   chainId: 1,
   contractAddr: '0x8888888883585b9a8202Db34D8b09d7252bfc61C',
-  tokenAddr: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  tokenAddr: '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
+  //tokenAddr: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // WETH
 
   // Order config
 
